4063e9f724cabcdc6ee1584d47a1e23a381eeb94
Adrian030304 <adrianromanenco1@gmail.com>
2025-12-05 22:35:16 +0200

    mock up route for unit conversion logic

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,31 @@ from app import app
 from flask import render_template, redirect, url_for, request, send_from_directory, send_file, Response, flash
 
 
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def index():
-    return render_template('index.html')
 
+    units = {
+        "mm": pow(10,-3),
+        "cm" : pow(10,-2),
+        "m" :  pow(10,0),
+        "km": pow(10,3),
+        "inch": 0.0254,
+        "foot": 0.0254 * 12,
+        "yard": 0.0254 * 12 * 3,
+        "mile": 0.0254 * 12 * 3 * 1760
+    }
+    print(units)
+
+    if request.method == 'POST':
+        value, _from, _to = request.form.values()
+        
+        value, _from, _to = float(value), float(_from), float(_to)
+        print(value, _from, _to)
+
+
+    return render_template('index.html', units = units)
+
+
+@app.route('/length')
+def length_page():
+    print(request.form)
---
+++ app/static/main.js
@@ -0,0 +1,8 @@
+const formBody = document.querySelector('.form-body');
+
+formBody.addEventListener('submit', (e) => {
+    console.log(e)
+    e.preventDefault();
+    // formBody.submit();
+
+})
