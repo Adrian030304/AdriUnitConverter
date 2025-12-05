0452c31620d2ffefead5768bdf573312b32466e1
Adrian030304 <adrianromanenco1@gmail.com>
2025-12-06 00:53:16 +0200

    js script for input validation and form submission

--- app/static/main.js
+++ app/static/main.js
@@ -1,8 +1,28 @@
 const formBody = document.querySelector('.form-body');
+const toUnit = document.getElementById('to-unit');
+const fromUnit = document.getElementById('from-unit');
+const errorMsg = document.getElementById('error');
+const inputVal = document.getElementById('val');
+const resetBtn = document.getElementById('reset');
 
-formBody.addEventListener('submit', (e) => {
-    console.log(e)
-    e.preventDefault();
-    // formBody.submit();
+if (formBody !== null) {
+    formBody.addEventListener('submit', (e) => {
+        console.log(e)
+        e.preventDefault();
+        formBody.submit();
 
-})
+    })
+}
+
+if (inputVal !== null) {
+    inputVal.addEventListener('input', (e) => {
+        let str = e.target.value;
+
+        const isValid = /^(\d+(\.\d*)?|\.\d+)$/.test(str);
+        if (str && (!isValid)) {
+            errorMsg.style.display = 'block';
+        } else {
+            errorMsg.style.display = 'none';
+        }
+    })
+}
